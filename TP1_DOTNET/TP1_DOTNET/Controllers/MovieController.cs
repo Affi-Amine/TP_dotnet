e07ddcfe91ce2258801cbe341794a9708cb680a6
Affi-Amine <affi.amin.work@gmail.com>
2023-12-03 13:55:51 +0000

    finished on working on the movie routing by month and year

---
+++ TP1_DOTNET/TP1_DOTNET/Controllers/MovieController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TP1_DOTNET.Models;
+
+namespace TP1_DOTNET.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IActionResult Index()
+        {
+            Movie movie = new Movie()
+            {
+                Name =
+            "movie 1"
+            };
+            List<Movie> movies = new List<Movie>()
+{
+new Movie{Name="movie 2"},
+new Movie{Name="movie 3"},
+};
+            return View(movies);
+        }
+        public IActionResult Edit(int id)
+        {
+            return Content("Test Id" + id);
+        }
+
+        [HttpGet("Movie/released/{year}/{month}")]
+        public IActionResult ByRelease(int month, int year)
+        {
+            return Content("Released in "+ month + " in " + year);
+        }
+    }
+}
---
+++ TP1_DOTNET/TP1_DOTNET/Models/Movie.cs
@@ -0,0 +1,8 @@
+﻿namespace TP1_DOTNET.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
